5403ef5f0497666898d5419558389ccf3bd773c8
Kishor-Kumar07 <kishorck00@gmail.com>
2020-08-11 21:26:06 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Publications from './publications'
+function App() {
+  return (
+    <div >
+      
+      <Publications />
+    </div>
+  );
+}
+
+export default App;
